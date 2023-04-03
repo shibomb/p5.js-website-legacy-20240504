d253185aa302b48835b9ffdd46049f9833026dec
tubokiti <potubokiti009@gmail.com>
2023-04-04 01:23:51 +0900

    00_Load_and_Display_Image.jsを日本語に翻訳

--- src/data/examples/ja/05_Image/00_Load_and_Display_Image.js
+++ src/data/examples/ja/05_Image/00_Load_and_Display_Image.js
@@ -1,23 +1,20 @@
 /*
- * @name Load and Display Image
- * @arialabel An astronaut on a planet with the same image in a smaller size in the bottom left quarter
- * @description Images can be loaded and displayed to the screen at their
- * actual size or any other size.
- * <p><em><span class="small"> To run this example locally, you will need an 
- * image file, and a running <a href="https://github.com/processing/p5.js/wiki/Local-server">
- * local server</a>.</span></em></p>
-
+ * @name 画像の読み込みと表示
+ * @arialabel 画面左下に宇宙飛行士の画像を半分の大きさで表示する。
+ * @description 画像は実際の大きさ、もしくは任意の大きさで表示することができます。
+ * <p><em><span class="small"> この例をローカル環境で実行するには画像ファイルと実行中の
+ * <a href="https://github.com/processing/p5.js/wiki/Local-server">ローカルサーバー</a>が必要です。</span></em></p>
  */
-let img; // Declare variable 'img'.
+let img; // 変数imgを宣言する。
 
 function setup() {
   createCanvas(720, 400);
-  img = loadImage('assets/moonwalk.jpg'); // Load the image
+  img = loadImage('assets/moonwalk.jpg'); // 画像ファイルをロードする。
 }
 
 function draw() {
-  // Displays the image at its actual size at point (0,0)
+  // 画像を実際の大きさで位置(0,0)に表示します。
   image(img, 0, 0);
-  // Displays the image at point (0, height/2) at half size
+  // 画像を半分の大きさで位置(0, height/2)に表示します。
   image(img, 0, height / 2, img.width / 2, img.height / 2);
 }
