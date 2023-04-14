a038aea00243e2bc3f85ae0a8d7b5a3edefea2fe
OOTSUBO_PC\potub <ootsubo.hi00@gmail.com>
2023-04-14 17:11:38 +0900

    06_Blur.js ~ 08_Brightness.jsまで翻訳

--- src/data/examples/ja/05_Image/07_EdgeDetection.js
+++ src/data/examples/ja/05_Image/07_EdgeDetection.js
@@ -1,94 +1,86 @@
 /*
- * @name Edge Detection
- * @arialabel Astronaut rendered in black and white on the left and a highly sharpened version of the image on the right
- * @description A high-pass filter sharpens an image. This program analyzes every pixel in an image in relation to the neighboring pixels to sharpen the image. 
- * <br><br><span class="small"><em>This example is ported from the <a href="https://processing.org/examples/edgedetection.html">Edge Detection example</a>
- * on the Processing website</em></span>
- */
-// this program analyzes every pixel in an image
-// in relation to the neighbouring pixels
-// to sharpen the image
+* @name エッジ検出
+* @arialabel 左側に白黒でレンダリングされた宇宙飛行士、右側には画像を高度にシャープ化したバージョンが表示されています
+* @description ハイパスフィルターにより画像をシャープ化します。このプログラムは、隣接するピクセルとの関係で画像内のすべてのピクセルを解析して、画像をシャープ化します。
+* <br><br><span class="small"><em>この例は、Processingウェブサイトの<a href="https://processing.org/examples/edgedetection.html">エッジ検出の例</a>から移植されたものです。</em></span>
+*/
+// このプログラムは、隣接するピクセルとの関係で画像内のすべてのピクセルを解析して、画像をシャープ化します
 
-// to consider all neighboring pixels we use a 3x3 array
-// and normalize these values
-// kernel is the 3x3 matrix of normalized values
+// 全ての隣接するピクセルを考慮するために、3x3の配列を使用し、これらの値を正規化します
+// カーネルは、正規化された値の3x3行列です
 let kernel = [[-1, -1, -1 ], [ -1,  9, -1 ], [-1, -1, -1 ]]; 
 
-// preload() runs once, before setup()
-// loadImage() needs to occur here instead of setup()
-// if loadImage() is called in setup(), the image won't appear 
-// since noLoop() restricts draw() to execute only once
-// (one execution of draw() is not enough time for the image to load),
-// preload() makes sure image is loaded before anything else occurs
+// preload()はsetup()の前に1回だけ実行されます
+// loadImage()はsetup()で呼び出すのではなく、ここで実行する必要があります
+// setup()でloadImage()を呼び出すと、noLoop()によりdraw()が1回しか実行されないため、
+// 画像が表示されません(1回のdraw()では画像をロードするための十分な時間がありません)。
+// preload()は、他の処理が実行される前に画像がロードされることを保証します
 function preload() {
-  // load the original image
+  // オリジナルの画像を読み込む
   img = loadImage("assets/rover.png"); 
 }
 
-// setup() runs after preload, once()
+// setup()はpreload()の後に1回実行されます
 function setup() {
-  // create canvas
+  // キャンバスを作成する
   createCanvas(710, 400);
-  // noLoop() makes draw() run only once, not in a loop
+  // noLoop()はdraw()を1回のみ実行し、ループしないようにします
   noLoop();
 }
 
-// draw() runs after setup(), normally on a loop
-// in this case it runs only once, because of noDraw()
+// draw()は通常、setup()の後にループ実行されます
+// 今回はnoLoop()のため1回だけ実行されます
 function draw() {
   
-  // place the original image on the upper left corner
+  // オリジナルの画像を左上隅に配置する
   image(img, 0, 0);
 
-  // create a new image, same dimensions as img
+  // imgと同じ寸法の新しい画像を作成する
   edgeImg = createImage(img.width, img.height);
   
-  // load its pixels
+  // 画像のピクセルを読み込む
   edgeImg.loadPixels();
-
   
-  // two for() loops, to iterate in x axis and y axis
-  // since the kernel assumes that the pixel
-  // has pixels above, under, left, and right
-  // we need to skip the first and last column and row
-  // x then goes from 1 to width - 1
-  // y then goes from 1 to height - 1
-
+  // x軸とy軸を反復処理するための2つのfor()ループ
+  // カーネルは、ピクセルが上下左右にピクセルを持っていることを前提としているため、
+  // 最初の列と行、最後の列と行をスキップする必要があります
+  // xは1から画像の幅-1までになります
+  // yは1から画像の高さ-1までになります
   for (let x = 1; x < img.width - 1; x++) {
     for (let y = 1; y < img.height - 1; y++) {
-      // kernel sum for the current pixel starts as 0
+      // 現在のピクセルのカーネルの合計は0から始まります
       let sum = 0; 
       
-      // kx, ky variables for iterating over the kernel
-      // kx, ky have three different values: -1, 0, 1
+      // カーネルを反復処理するためのkx、ky変数
+      // kx、kyには3つの異なる値があります：-1、0、1
       for (kx = -1; kx <= 1; kx++) {
         for (ky = -1; ky <= 1; ky++) {
           
           let xpos = x + kx;
           let ypos = y + ky;
           let pos = (y + ky)*img.width + (x + kx);
-          // since our image is grayscale, 
-          // RGB values are identical
-          // we retrieve the red value for this example
+          // 画像がグレースケールであるため、
+          // RGB値は同じです
+          // この例では赤の値を取得します
           let val = red(img.get(xpos, ypos));
-          // accumulate the  kernel sum
-          // kernel is a 3x3 matrix
-          // kx and ky have values -1, 0, 1
-          // if we add 1 to kx and ky, we get 0, 1, 2
-          // with that we can use it to iterate over kernel
-          // and calculate the accumulated sum
+          // カーネルの合計を蓄積する
+          // カーネルは3x3の行列です
+          // kxとkyには-1、0、1の値があります
+          // kxとkyに1を加えると、0、1、2が得られます
+          // それを使用してカーネルを反復処理し、
+          // 累積和を計算できます
           sum += kernel[ky+1][kx+1] * val;
         }
       }
       
-      // set the pixel value of the edgeImg 
+      // edgeImgのピクセル値を設定します
       edgeImg.set(x, y, color(sum, sum, sum));
     }
   }
   
-  // updatePixels() to write the changes on edgeImg
+  // edgeImgの変更を書き込みます
   edgeImg.updatePixels();
   
-  // draw edgeImg at the right of the original image
+  // 元の画像の右にedgeImg（エッジ検出した画像）を描画します
   image(edgeImg, img.width, 0);
 }
--- src/data/examples/ja/05_Image/08_Brightness.js
+++ src/data/examples/ja/05_Image/08_Brightness.js
@@ -1,23 +1,20 @@
 /*
- * @name Brightness
- * @arialabel Astronaut rendered in black and white is covered with a black screen. The user’s mouse acts as a flashlight and parts of the image are illuminated as the user’s mouse travels over
- * @description This program adjusts the brightness of a part of the image by calculating the distance of each pixel to the mouse.
- * <br><br><span class="small"><em>This example is ported from the <a href="https://processing.org/examples/brightness.html">Brightness example</a>
- * on the Processing website</em></span>
- */
-// This program adjusts the brightness 
-// of a part of the image by 
-// calculating the distance of 
-// each pixel to the mouse.
+* @name 明度(Brightness)
+* @arialabel 白黒で描かれた宇宙飛行士のイメージに黒いスクリーンがかかります。ユーザーのマウスが懐中電灯のように機能し、マウスを動かすことで画像の一部分が照らされます。
+* @description このプログラムは、各ピクセルとマウスとの距離を計算して、画像の一部の明るさを調整します。
+* <br><br><span class="small"><em>この例はProcessingウェブサイトの<a href="https://processing.org/examples/brightness.html">Brightness example</a>を移植したものです。</em></span>
+*/
+// このプログラムは、各ピクセルとマウスとの距離を計算して、
+// 画像の一部の明るさを調整します。
 let img;
-// preload() runs once, before setup()
-// loadImage() needs to occur here instead of setup()
-// preload() makes sure image is loaded before anything else occurs
+// preload()はsetup()の前に1回実行されます。
+// loadImage()は、setup()ではなくここで実行する必要があります。
+// preload()は、他の処理が実行される前に画像が読み込まれることを保証します。
 function preload() {
-  // load the original image
+  // オリジナルの画像を読み込む。
   img = loadImage("assets/rover_wide.jpg");  
 }
-// setup() runs after preload, once()
+// setup()はpreload()の後に1回実行されます。
 function setup() {
   createCanvas(710, 400);
   pixelDensity(1);
@@ -26,38 +23,38 @@ function setup() {
 
 function draw() {
     image(img,0,0);
-    // Only need to load the pixels[] array once, because we're only
-    // manipulating pixels[] inside draw(), not drawing shapes.
+    // pixels[]配列をロードする必要があるのは1回だけです。
+    // なぜなら形状を描くのではなく、draw()内でpixels[]を操作するためだからです。
     loadPixels();
-    // We must also call loadPixels() on the PImage since we are going to read its pixels.
+    // この画像のピクセルを読み取るためにloadPixels()を呼び出す必要があります。
     img.loadPixels();
     for (let x = 0; x < img.width; x++) {
         for (let y = 0; y < img.height; y++ ) {
-        // Calculate the 1D location from a 2D grid
+        // 2Dグリッドから1Dの位置を計算する
         let loc = (x + y*img.width)*4;
-        // Get the R,G,B values from image
+        // 画像からR、G、Bの値を取得する（今回はRのみ）
         let r,g,b;
         r = img.pixels[loc];
         // g = img.pixels[loc+1];
         // b = img.pixels[loc+2];
-        // Calculate an amount to change brightness based on proximity to the mouse
-        // The closer the pixel is to the mouse, the lower the value of "distance"
+        // マウスに近い距離に基づいて明るさを変更する量を計算する
+        // ピクセルがマウスに近いほど、「distance」の値が低くなります
         let maxdist = 50;//dist(0,0,width,height);
         let d = dist(x, y, mouseX, mouseY);
         let adjustbrightness = 255*(maxdist-d)/maxdist;
         r += adjustbrightness;
         // g += adjustbrightness;
         // b += adjustbrightness;
-        // Constrain RGB to make sure they are within 0-255 color range
+        // Rが0-255のカラー範囲内に収まるように設定する
         r = constrain(r, 0, 255);
         // g = constrain(g, 0, 255);
         // b = constrain(b, 0, 255);
-        // Make a new color and set pixel in the window
+        // 新しい色を作成して、ウィンドウ内のピクセルを設定する
         let pixloc = (y*width + x)*4;
         pixels[pixloc] = r;
         pixels[pixloc+1] = r;
         pixels[pixloc+2] = r;
-        pixels[pixloc+3] = 255; // Always have to set alpha
+        pixels[pixloc+3] = 255; // アルファ値は255固定
         }
     }
     updatePixels();
