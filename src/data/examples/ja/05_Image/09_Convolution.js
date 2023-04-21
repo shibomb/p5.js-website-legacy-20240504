ee3b767d1d63b51cdbcb9e4b85fc2e2e88d31bf4
OOTSUBO_PC\potub <ootsubo.hi00@gmail.com>
2023-04-21 12:39:02 +0900

    09_Convolution.jsと10_Copy_Method.jsの翻訳を他のレビュー結果を参考に修正

--- src/data/examples/ja/05_Image/09_Convolution.js
+++ src/data/examples/ja/05_Image/09_Convolution.js
@@ -1,15 +1,15 @@
 /*
 * @name 畳み込み(Convolution)
-* @arialabel 背景に宇宙飛行士の画像を表示し、ユーザーのマウスが動くにつれ画像の鮮明度を上げる正方形のセクションも動きます。
-* @description 画像の一部に畳み込み行列を適用します。マウスを移動させると、異なる部分にフィルタを適用できます。この例はProcessing用に<a href="https://processing.org/examples/convolution.html" target="blank">Dan Shiffmanの例</a>を移植したものです。ソース中のコメントはDanによって書かれたものを翻訳しています。
-* <p><em><span class="small">この例をローカルで実行するには、画像ファイルと<a href="https://github.com/processing/p5.js/wiki/Local-server">ローカルサーバ</a>が必要です。</span></em></p>
+* @arialabel 宇宙飛行士の画像を背景に表示し、ユーザーのマウスに追従して画像の鮮明度を上げる正方形のセクションも動きます。
+* @description 画像の一部に畳み込み行列を適用します。マウスを移動させると別の箇所にフィルターを適用できます。この例は、Processingウェブサイトの<a href="https://processing.org/examples/convolution.html" target="blank">Convolution example</a>を移植したものです。
+* <p><em><span class="small">この例をローカルで実行するには画像ファイルと<a href="https://github.com/processing/p5.js/wiki/Local-server">ローカルサーバー</a>が必要です。</span></em></p>
 */
  
 let img;
 let w = 80;
 
-// 多数の畳み込み行列で画像を畳み込むことで、さまざまな効果を生み出すことができます。
-// これはハイパスフィルタで、エッジを強調します。
+// 3x3の畳み込み行列で画像を畳み込むことで、さまざまな効果を生み出すことができます。
+// これはハイパスフィルターでエッジを強調します。
 const matrix = [ [ -1, -1, -1 ],
                  [ -1,  9, -1 ],
                  [ -1, -1, -1 ] ]; 
@@ -23,7 +23,7 @@ function setup() {
   img.loadPixels();
 
   // pixelDensity(1) はピクセル密度を表示密度にスケーリングしないためのものです。
-  // 詳細については、pixelDensity()のリファレンスを参照してください。
+  // 詳細についてはpixelDensity()のリファレンスを参照してください。
   pixelDensity(1);
 }
 
@@ -31,7 +31,7 @@ function draw() {
   // 画像の一部分だけを処理する予定なので、まずは全体を背景として設定しましょう。
   background(img);
 
-  // 処理をする範囲の小さな矩形を計算します。
+  // フィルターを適用する正方形のセクションを計算します。
   const xstart = constrain(mouseX - w/2, 0, img.width);
   const ystart = constrain(mouseY - w/2, 0, img.height);
   const xend = constrain(mouseX + w/2, 0, img.width);
@@ -39,12 +39,12 @@ function draw() {
   const matrixsize = 3;
 
   loadPixels();
-  // 範囲内のすべてのピクセルに対してループを開始します。
+  // 範囲内のすべてのピクセルに対して処理を開始します。
   for (let x = xstart; x < xend; x++) {
     for (let y = ystart; y < yend; y++ ) {
       let c = convolution(x, y, matrix, matrixsize, img);
       
-      // 変数 c からRGBAの値を取得して、pixels()を更新します。
+      // 変数「c」からRGBAの値を取得してピクセルを更新します。
       let loc = (x + y*img.width) * 4;
       pixels[loc] = red(c);
       pixels[loc + 1] = green(c);
@@ -68,7 +68,7 @@ function convolution(x, y, matrix, matrixsize, img) {
       const yloc = (y + j - offset);
       let loc = (xloc + img.width * yloc) * 4;
 
-      // 画像から外れていないことを確認しています。 we could do better here
+      // 画像の範囲内かどうかを確認しています。 we could do better here
       loc = constrain(loc, 0 , img.pixels.length - 1);
 
       // 畳み込みの計算
@@ -78,11 +78,11 @@ function convolution(x, y, matrix, matrixsize, img) {
       btotal += (img.pixels[loc + 2]) * matrix[i][j];
     }
   }
-  // RGBが0~255に収まっていることを確認しています。
+  // RGB値を0~255に制限します。
   rtotal = constrain(rtotal, 0, 255);
   gtotal = constrain(gtotal, 0, 255);
   btotal = constrain(btotal, 0, 255);
   
-  // 畳み込みをした色を返す
+  // 畳み込み処理をした結果の色を返します。
   return color(rtotal, gtotal, btotal);
 } 
