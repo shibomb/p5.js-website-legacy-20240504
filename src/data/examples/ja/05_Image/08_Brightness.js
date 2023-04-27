d6c6b0cd9da0cf59dd865615134f86eabf962253
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-04-28 01:25:25 +0900

    fix: 「例」を「サンプル」に変更

--- src/data/examples/ja/05_Image/08_Brightness.js
+++ src/data/examples/ja/05_Image/08_Brightness.js
@@ -1,9 +1,9 @@
 /*
-* @name 明度
-* @arialabel 白黒で描かれた宇宙飛行士のイメージが黒いスクリーンで覆われています。ユーザーのマウスが懐中電灯のように機能し、マウスを動かすことで画像の一部分が照らされます。
-* @description このプログラムは、各ピクセルとマウスとの距離を計算して、画像の一部の明るさを調整します。
-* <br><br><span class="small"><em>この例は、Processingウェブサイトの<a href="https://processing.org/examples/brightness.html">Brightness example</a>を移植したものです。</em></span>
-*/
+ * @name 明度
+ * @arialabel 白黒で描かれた宇宙飛行士のイメージが黒いスクリーンで覆われています。ユーザーのマウスが懐中電灯のように機能し、マウスを動かすことで画像の一部分が照らされます。
+ * @description このプログラムは、各ピクセルとマウスとの距離を計算して、画像の一部の明るさを調整します。
+ * <br><br><span class="small"><em>このサンプルは、Processingウェブサイトの<a href="https://processing.org/examples/brightness.html">Brightnessのサンプル</a>を移植したものです。</em></span>
+ */
 // このプログラムは、各ピクセルとマウスとの距離を計算して、
 // 画像の一部の明るさを調整します。
 let img;
@@ -12,7 +12,7 @@ let img;
 // preload()は、他の処理が実行される前に画像が読み込まれることを保証します。
 function preload() {
   // オリジナルの画像を読み込む
-  img = loadImage("assets/rover_wide.jpg");  
+  img = loadImage('assets/rover_wide.jpg');
 }
 // setup()はpreload()の後に1回実行されます。
 function setup() {
@@ -22,40 +22,40 @@ function setup() {
 }
 
 function draw() {
-    image(img,0,0);
-    // pixels[]配列を読み込む必要があるのは1回だけです。
-    // 形状を描くのではなく、draw()内でpixels[]を操作するためです。
-    loadPixels();
-    // この画像のピクセルを読み取るためにloadPixels()を呼び出す必要があります。
-    img.loadPixels();
-    for (let x = 0; x < img.width; x++) {
-        for (let y = 0; y < img.height; y++ ) {
-        // 2Dグリッドから1Dの位置を計算する
-        let loc = (x + y*img.width)*4;
-        // 画像からR、G、Bの値を取得する（今回はRのみ）
-        let r,g,b;
-        r = img.pixels[loc];
-        // g = img.pixels[loc+1];
-        // b = img.pixels[loc+2];
-        // マウスに近い距離に基づいて明るさを変更する量を計算する
-        // ピクセルがマウスに近いほど、「distance」の値が低くなります。
-        let maxdist = 50;//dist(0,0,width,height);
-        let d = dist(x, y, mouseX, mouseY);
-        let adjustbrightness = 255*(maxdist-d)/maxdist;
-        r += adjustbrightness;
-        // g += adjustbrightness;
-        // b += adjustbrightness;
-        // Rが0-255のカラー範囲内に収まるように設定する
-        r = constrain(r, 0, 255);
-        // g = constrain(g, 0, 255);
-        // b = constrain(b, 0, 255);
-        // 新しい色を作成して、ウィンドウ内のピクセルを設定する
-        let pixloc = (y*width + x)*4;
-        pixels[pixloc] = r;
-        pixels[pixloc+1] = r;
-        pixels[pixloc+2] = r;
-        pixels[pixloc+3] = 255; // アルファ値は255固定
-        }
+  image(img, 0, 0);
+  // pixels[]配列を読み込む必要があるのは1回だけです。
+  // 形状を描くのではなく、draw()内でpixels[]を操作するためです。
+  loadPixels();
+  // この画像のピクセルを読み取るためにloadPixels()を呼び出す必要があります。
+  img.loadPixels();
+  for (let x = 0; x < img.width; x++) {
+    for (let y = 0; y < img.height; y++) {
+      // 2Dグリッドから1Dの位置を計算する
+      let loc = (x + y * img.width) * 4;
+      // 画像からR、G、Bの値を取得する（今回はRのみ）
+      let r, g, b;
+      r = img.pixels[loc];
+      // g = img.pixels[loc+1];
+      // b = img.pixels[loc+2];
+      // マウスに近い距離に基づいて明るさを変更する量を計算する
+      // ピクセルがマウスに近いほど、「distance」の値が低くなります。
+      let maxdist = 50; //dist(0,0,width,height);
+      let d = dist(x, y, mouseX, mouseY);
+      let adjustbrightness = (255 * (maxdist - d)) / maxdist;
+      r += adjustbrightness;
+      // g += adjustbrightness;
+      // b += adjustbrightness;
+      // Rが0-255のカラー範囲内に収まるように設定する
+      r = constrain(r, 0, 255);
+      // g = constrain(g, 0, 255);
+      // b = constrain(b, 0, 255);
+      // 新しい色を作成して、ウィンドウ内のピクセルを設定する
+      let pixloc = (y * width + x) * 4;
+      pixels[pixloc] = r;
+      pixels[pixloc + 1] = r;
+      pixels[pixloc + 2] = r;
+      pixels[pixloc + 3] = 255; // アルファ値は255固定
     }
-    updatePixels();
+  }
+  updatePixels();
 }
--- src/data/examples/ja/05_Image/10_Copy_Method.js
+++ src/data/examples/ja/05_Image/10_Copy_Method.js
@@ -1,18 +1,18 @@
 /*
-* @name Copy()メソッド
-* @arialabel 白黒で描かれたオウムを表示します。マウスカーソルがペイントブラシとなり、画像の上でマウスを押し続けるとその領域に色が付きます。
-* @frame 600,400
-* @description copy()メソッドを使用して画像の着色をシミュレートする方法の例
-*/
+ * @name Copy()メソッド
+ * @arialabel 白黒で描かれたオウムを表示します。マウスカーソルがペイントブラシとなり、画像の上でマウスを押し続けるとその領域に色が付きます。
+ * @frame 600,400
+ * @description copy()メソッドを使用して画像の着色をシミュレートする方法のサンプルです。
+ */
 let draft, ready;
 function preload() {
-  ready = loadImage("assets/parrot-color.png");
-  draft = loadImage("assets/parrot-bw.png");
+  ready = loadImage('assets/parrot-color.png');
+  draft = loadImage('assets/parrot-bw.png');
 }
 function setup() {
   createCanvas(600, 400);
   noCursor();
-  cursor("assets/brush.png", 20, -10);
+  cursor('assets/brush.png', 20, -10);
   image(ready, 0, 0);
   image(draft, 0, 0);
 }
