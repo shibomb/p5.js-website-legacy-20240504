72b2e4a2a89a18f4f08a008bac9df4d218355239
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-01 03:21:44 +0900

    review and modify examples/ja/09_Simulate/08_Spring

--- src/data/examples/ja/09_Simulate/08_Spring.js
+++ src/data/examples/ja/09_Simulate/08_Spring.js
@@ -1,8 +1,8 @@
 /*
  * @name スプリング
- * @arialabel グレーの背景に黒い垂直長方形の上に薄灰色の水平長方形があり、ユーザーは水平長方形を上下に動かすことができます。水平長方形を離すと、システムはスプリングのように動き、垂直長方形が水平長方形が上下に動くにつれて伸縮します。
+ * @arialabel グレーの背景に黒い縦長の長方形があり、上に薄いグレーの横長の長方形が載っています。ユーザーは横長の長方形を上下に動かすことができます。横長の長方形を離すと、システムはスプリングのように動き、横長の長方形が上下に動くにつれて縦長の長方形が伸縮します。
  * @frame 710, 400
- * @description 水平バーをクリックしてドラッグし、スプリングを起動します。
+ * @description 水平バーをクリックし、ドラッグして離すと、スプリングが起動します。
  */
 // 上部バーのスプリング描画定数
 let springHeight = 32,
@@ -13,13 +13,13 @@ let springHeight = 32,
     over = false,
     move = false;
 
-// スプリングシミュレーション定数
+// スプリング シミュレーション定数
 let M = 0.8, // 質量
     K = 0.2, // スプリング定数
     D = 0.92, // ダンピング
     R = 150; // 安定位置
 
-// スプリングシミュレーション変数
+// スプリング シミュレーション変数
     let ps = R, // 位置
     vs = 0.0, // 速度
     as = 0, // 加速度
@@ -40,12 +40,12 @@ function draw() {
 }
 
 function drawSpring() {
-  // ベースを描画
+  // ベースを描画します。
   fill(0.2);
   let baseWidth = 0.5 * ps + -8;
   rect(width / 2 - baseWidth, ps + springHeight, width / 2 + baseWidth, height);
 
-  // 色を設定し、上部バーを描画
+  // 色を設定し、上部バーを描画します。
   if (over || move) {
     fill(255);
   } else {
@@ -56,26 +56,26 @@ function drawSpring() {
 }
 
 function updateSpring() {
-  // スプリング位置を更新
+  // スプリングの位置を更新します。
   if ( !move ) {
     f = -K * ( ps - R );  // f=-ky
-    as = f / M;           // 加速度を設定, f=ma == a=f/m
-    vs = D * (vs + as);   // 速度を設定
-    ps = ps + vs;         // 位置を更新
+    as = f / M;           // 加速度（f=ma == a=f/m）を設定します。
+    vs = D * (vs + as);   // 速度を設定します。
+    ps = ps + vs;         // 位置を更新します。
   }
 
   if (abs(vs) < 0.1) {
     vs = 0.0;
   }
 
-  // マウスが上部バーの上にあるかどうかをテスト
+  // マウスが上部バーの上にあるかどうかをテストします。
   if (mouseX > left && mouseX < right && mouseY > ps && mouseY < ps + springHeight) {
     over = true;
   } else {
     over = false;
   }
 
-  // 上部バーの位置を設定し、制約をかける
+  // 上部バーの位置を設定し、制約をかけます。
   if (move) {
     ps = mouseY - springHeight / 2;
     ps = constrain(ps, minHeight, maxHeight);
