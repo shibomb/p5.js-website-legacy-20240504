17ca17028d75135c62cf1f745f87813be859eb31
kkeeth <zensin0082@gmail.com>
2023-05-14 15:21:05 +0900

    fix: reviewed comments

--- src/data/examples/ja/00_Structure/04_No_Loop.js
+++ src/data/examples/ja/00_Structure/04_No_Loop.js
@@ -1,12 +1,12 @@
 /*
- * @name No Loop
- * @arialabel 黒背景の中央を白の水平線が横切る
+ * @name ループ停止
+ * @arialabel 背景の中央を横切る白の水平線
  * @description noLoop()関数を用いるとdraw()関数を一度のみ実行することができます。
  * noLoop()関数を呼ばなければ、draw()関数内のコードは繰り返し実行されます。
  */
 let y;
 
-// setup() 関数内のステートメントは
+// setup()関数内のステートメントは
 // プログラム開始時に一度だけ実行されます。
 function setup() {
   // createCanvas ステートメントはなるべく最初に記述
