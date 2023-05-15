fb6874a32ad26d14c038708bbfa4e23aa44e5957
kkeeth <zensin0082@gmail.com>
2023-05-15 22:59:46 +0900

    feat: translated create graphics page in details

--- src/data/examples/ja/03_Arrays/00_Array.js
+++ src/data/examples/ja/03_Arrays/00_Array.js
@@ -1,13 +1,13 @@
 /*
- * @name Array
- * @arialabel Vertical lines are graphed across a white background to visualize the values of a cosine curve
- * @description An array is a list of data. Each piece of data in an array
- * is identified by an index number representing its position in
- * the array. Arrays are zero based, which means that the first
- * element in the array is [0], the second element is [1], and so on.
- * In this example, an array named "coswave" is created and
- * filled with the cosine values. This data is displayed three
- * separate ways on the screen.
+ * @name 配列
+ * @arialabel 垂直な線が白背景上を横切り、棒グラフのようにコサイン曲線の値を可視化しています。
+ * @description 配列はデータの一覧を意味します。配列内のデータの値ひとつひとつは、
+ * インデックスの数字を用いて配列内の自分の位置を特定できます。
+ * 配列はゼロ ベースです。これはつまり
+ * 配列の最初の要素は[0]、２番目の要素は[1]、これ以降も同様であることを意味します。
+ * このサンプルでは、 "coswave"という名前の配列が作成され
+ * 初期値をコサインの値で埋められます。このデータは画面上に
+ * ３分割して表示されます。
  */
 let coswave = [];
 
