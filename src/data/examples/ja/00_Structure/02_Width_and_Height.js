957075c794ab4b3e3b1a2152b1d9f4aa117657a4
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-16 11:35:35 +0900

    fix: add space characters around alphabet

--- src/data/examples/ja/00_Structure/02_Width_and_Height.js
+++ src/data/examples/ja/00_Structure/02_Width_and_Height.js
@@ -1,8 +1,8 @@
 /*
- * @name widthとHeight
+ * @name width と Height
  * @arialabel 灰色と緑色の水平線のパターンです。左半分には白の垂直線も含まれ、2つの三角形に分けられています。片方は緑の縞模様、もう一方は白の縞模様が目立つように描画されています。
  * @description 変数「width」と「height」には、
- * createCanvas()関数で定義された表示ウィンドウの幅と高さが格納されます。
+ * createCanvas() 関数で定義された表示ウィンドウの幅と高さが格納されます。
  */
 function setup() {
   createCanvas(720, 400);
