6ad144991677b6a0d478b42a373cd9161d206cd0
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-18 02:31:50 +0900

    fix: modify the translation for 02_Transparency in accordance with our style guide

--- src/data/examples/ja/05_Image/02_Transparency.js
+++ src/data/examples/ja/05_Image/02_Transparency.js
@@ -1,7 +1,7 @@
 /*
  * @name 画像の透過
  * @arialabel 宇宙飛行士の画像を背景に表示し、半透明な宇宙飛行士の画像が上に表示されています。半透明の画像はマウスの水平方向に移動します。
- * @description 画像を左右に動かすには、画像上でマウスを左右に動かします。このプログラムは、tint()関数で画像のアルファ値を変更することで、1つの画像を別の画像にオーバーレイします。
+ * @description 画像を左右に動かすには、画像上でマウスを左右に動かします。このプログラムは、tint() 関数で画像のアルファ値を変更することで、1つの画像を別の画像にオーバーレイします。
  * <p><em><span class="small"> このサンプルをローカルで実行するには画像ファイルと実行中の<a href="https://github.com/processing/p5.js/wiki/Local-server">
  * ローカルサーバー</a>が必要です。</span></em></p>
  */
@@ -11,13 +11,13 @@ let easing = 0.05;
 
 function setup() {
   createCanvas(720, 400);
-  img = loadImage('assets/moonwalk.jpg'); // プログラムに画像を読み込む
+  img = loadImage('assets/moonwalk.jpg'); // プログラムに画像を読み込みます。
 }
 
 function draw() {
-  image(img, 0, 0); // 100%の不透明度で表示する
+  image(img, 0, 0); // 100%の不透明度で表示します。
   let dx = mouseX - img.width / 2 - offset;
   offset += dx * easing;
-  tint(255, 127); // 50%の不透明度で表示する
+  tint(255, 127); // 50%の不透明度で表示します。
   image(img, offset, 0);
 }
