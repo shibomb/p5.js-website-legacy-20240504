3eb0bf0f1b90547ef6c1282be215177b01a1a60b
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-18 21:10:24 +0900

    fix: modify the translation in accordance with our style guide

--- src/data/examples/ja/09_Simulate/06_Spirograph.js
+++ src/data/examples/ja/09_Simulate/06_Spirograph.js
@@ -3,8 +3,8 @@
  * @arialabel グレーの背景に黒い円のアウトラインが互いに連動して回転しているスパイログラフが作成されます。ユーザーがスペースキーを押すと、背景が白くなり、さまざまなサイズの円の軌跡がインディゴ色で表示されます。
  * @description このスケッチは、シンプルな変換を使用して、
  * 互いに連動する円（サインと呼ばれる）をもつスパイログラフのような効果を作成します。
- * スペースキーを押すと、トレースと仕組みのジオメトリー（サインの動き）の表示を切り替えられます。<br>
- * サンプル作成者：<a href='http://lukedubois.com/' target='_blank'>R. Luke DuBois</a><br>
+ * スペースキーを押すと、トレースと仕組みのジオメトリ（サインの動き）の表示を切り替えられます。<br>
+ * サンプル作成者: <a href='http://lukedubois.com/' target='_blank'>R. Luke DuBois</a><br>
  * <a href='http://en.wikipedia.org/wiki/Spirograph'>http://en.wikipedia.org/wiki/Spirograph</a>
  */
 let NUMSINES = 20; // 一度に描画するサインの数
@@ -12,7 +12,7 @@ let sines = new Array(NUMSINES); // 現在の角度を保持する配列
 let rad; // 中心のサインの初期半径値
 let i; // カウンター変数
 
-// これらを操作して、何が起こっているかを理解しましょう：
+// これらを操作して、何が起こっているかを理解しましょう:
 let fund = 0.005; // 中心のサインの速度
 let ratio = 1; // 追加されるサインの速度の乗数
 let alpha = 50; // トレースシステムの不透明度
@@ -42,7 +42,7 @@ function draw() {
   translate(width / 2, height / 2); // 画面の中心に移動します。
 
   for (let i = 0; i < sines.length; i++) {
-    let erad = 0; // 円内の小さな"点"の半径...これがトレース時の'ペン'になります。
+    let erad = 0; // 円内の小さな「点」の半径...これがトレース時の「ペン」になります。
     // トレースの設定
     if (trace) {
       stroke(0, 0, 255 * (float(i) / sines.length), alpha); // 青色
@@ -50,7 +50,7 @@ function draw() {
       erad = 5.0 * (1.0 - float(i) / sines.length); // ペンの太さは、どのサインに関連しているかによって決まります。
     }
     let radius = rad / (i + 1); // 円自体の半径
-    rotate(sines[i]); // 円を回転させる
+    rotate(sines[i]); // 円を回転させます。
     if (!trace) ellipse(0, 0, radius * 2, radius * 2); // シミュレーション中なら、サインを描画します。
     push(); // ひとつ上のレベルへ移動します。
     translate(0, radius); // サインの端まで移動します。
