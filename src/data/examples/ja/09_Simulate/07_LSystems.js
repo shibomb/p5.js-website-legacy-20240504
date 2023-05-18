de5ac70a449767a7b3cf2a36e1b62e1471f6436a
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-18 21:34:41 +0900

    fix: put space characters before and behind alphabet words

--- src/data/examples/ja/09_Simulate/07_LSystems.js
+++ src/data/examples/ja/09_Simulate/07_LSystems.js
@@ -1,8 +1,8 @@
 /*
- * @name Lシステム
+ * @name L システム
  * @arialabel さまざまな色とパターンの円が、直角に曲がる黒い線でつながれてグリッド状に整列した形で画面に描かれます。
- * @description このスケッチは、Lindenmayerシステム（Lシステムとも呼ばれます）に基づいた自動描画を作成します。
- * Lシステムは、手続き型グラフィックスで
+ * @description このスケッチは、Lindenmayer システム（L システムとも呼ばれます）に基づいた自動描画を作成します。
+ * L システムは、手続き型グラフィックスで
  * 自然、幾何学的、または興味深い「フラクタル風」のパターンを作成するためによく使用されます。<br>
  * サンプル作成者：<a href='http://lukedubois.com/' target='_blank'>R. Luke DuBois</a><br>
  * <a href='https://en.wikipedia.org/wiki/L-system'>https://en.wikipedia.org/wiki/L-system</a>
@@ -13,25 +13,25 @@ let currentangle = 0; // タートルが向いている方向
 let step = 20; // タートルが「F」で動く量
 let angle = 90; // タートルが「-」または「+」で回転する角度
 
-// Lindenmayer関連の変数（Lシステム）
+// Lindenmayer 関連の変数（L システム）
 let thestring = 'A'; // 文字列の開始点（「原則」）
 let numloops = 5; // 事前に計算する反復回数
 let therules = []; // ルールの配列
 therules[0] = ['A', '-BF+AFA+FB-']; // 最初のルール
 therules[1] = ['B', '+AF-BFB-FA+']; // 二番目のルール
 
-let whereinstring = 0; // Lシステム内のどこにいるか
+let whereinstring = 0; // L システム内のどこにいるか
 
 function setup() {
   createCanvas(710, 400);
   background(255);
   stroke(0, 0, 0, 255);
 
-  // xとyの位置を左下隅に設定します。
+  // x と y の位置を左下隅に設定します。
   x = 0;
   y = height-1;
 
-  // Lシステムを計算します。
+  // L システムを計算します。
   for (let i = 0; i < numloops; i++) {
     thestring = lindenmayer(thestring);
   }
@@ -49,7 +49,7 @@ function draw() {
 
 }
 
-// Lシステムを解釈します。
+// L システムを解釈します。
 function lindenmayer(s) {
   let outputstring = ''; // 空の出力文字列を開始します。
 
@@ -60,7 +60,7 @@ function lindenmayer(s) {
       if (s[i] == therules[j][0])  {
         outputstring += therules[j][1]; // 置換文字列を書き込みます。
         ismatch = 1; // 一致したので、シンボルはコピーしません。
-        break; // このfor()ループを抜けます。
+        break; // この for() ループを抜けます。
       }
     }
     // 一致しなかった場合、シンボルをコピーします。
@@ -74,7 +74,7 @@ function lindenmayer(s) {
 function drawIt(k) {
 
   if (k=='F') { // 前進します。
-    // stepとcurrentangleに基づいて極座標から直交座標に変換します：
+    // step と currentangle に基づいて極座標から直交座標に変換します：
     let x1 = x + step*cos(radians(currentangle));
     let y1 = y + step*sin(radians(currentangle));
     line(x, y, x1, y1); // 古い座標と新しい座標を接続します。
