c1aff6609bab073eb2b13e9cdf439e68512fc885
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-18 16:53:57 +0900

    fix: add space characters around alphabet words in the translation for 07_EdgeDetection

--- src/data/examples/ja/05_Image/07_EdgeDetection.js
+++ src/data/examples/ja/05_Image/07_EdgeDetection.js
@@ -7,82 +7,82 @@
 // このプログラムは、隣接するピクセルとの関係で画像内のすべてのピクセルを分析して、画像をシャープ化します。
 
 // 全ての隣接するピクセルを考慮するために、3x3の配列を使用し、これらの値を正規化します。
-// kernelは正規化された値の3x3行列です。
+// kernel は正規化された値の3x3行列です。
 let kernel = [
   [-1, -1, -1],
   [-1, 9, -1],
   [-1, -1, -1],
 ];
 
-// preload()はsetup()の前に1回だけ実行されます。
-// loadImage()はsetup()で呼び出すのではなく、ここで実行する必要があります。
-// setup()でloadImage()を呼び出すと、noLoop()によりdraw()が1回しか実行されないため、
-// 画像が表示されません（1回のdraw()では画像を読み込むための十分な時間がありません）。
-// preload()は、他の処理が実行される前に画像が読み込まれることを保証します。
+// preload() は setup() の前に1回だけ実行されます。
+// loadImage() は setup() で呼び出すのではなく、ここで実行する必要があります。
+// setup() で loadImage() を呼び出すと、noLoop() により draw() が1回しか実行されないため、
+// 画像が表示されません（1回の draw() では画像を読み込むための十分な時間がありません）。
+// preload() は、他の処理が実行される前に画像が読み込まれることを保証します。
 function preload() {
   // オリジナルの画像を読み込む
   img = loadImage('assets/rover.png');
 }
 
-// setup()はpreload()の後に1回実行されます。
+// setup() は preload() の後に1回実行されます。
 function setup() {
   // キャンバスを作成する
   createCanvas(710, 400);
-  // noLoop()はdraw()を1回のみ実行し、ループしないようにします。
+  // noLoop() は draw() を1回のみ実行し、ループしないようにします。
   noLoop();
 }
 
-// draw()は通常、setup()の後にループ実行されます。
-// 今回はnoLoop()のため1回だけ実行されます。
+// draw() は通常、setup() の後にループ実行されます。
+// 今回は noLoop() のため1回だけ実行されます。
 function draw() {
   // オリジナルの画像を左上隅に配置する
   image(img, 0, 0);
 
-  // imgと同じ寸法の新しい画像を作成する
+  // img と同じ寸法の新しい画像を作成する
   edgeImg = createImage(img.width, img.height);
 
   // 画像のピクセルを読み込む
   edgeImg.loadPixels();
 
-  // x軸とy軸を反復処理するための2つのfor()ループ
-  // kernelはピクセルが上下左右にピクセルを持っていることを前提としているため、
+  // x 軸と y 軸を反復処理するための2つの for() ループ
+  // kernel はピクセルが上下左右にピクセルを持っていることを前提としているため、
   // 最初の列と行、最後の列と行をスキップする必要があります。
-  // xは1から画像の幅-1までになります。
-  // yは1から画像の高さ-1までになります。
+  // x は1から画像の幅-1までになります。
+  // y は1から画像の高さ-1までになります。
   for (let x = 1; x < img.width - 1; x++) {
     for (let y = 1; y < img.height - 1; y++) {
-      // 現在のピクセルのkernelの合計は0から始まります。
+      // 現在のピクセルの kernel の合計は0から始まります。
       let sum = 0;
 
-      // kernelを反復処理するためのkx、ky変数
-      // kx、kyには3つの異なる値があります：-1、0、1
+      // kernel を反復処理するための kx、ky 変数
+      // kx、ky には3つの異なる値があります：-1、0、1
       for (kx = -1; kx <= 1; kx++) {
         for (ky = -1; ky <= 1; ky++) {
           let xpos = x + kx;
           let ypos = y + ky;
           let pos = (y + ky) * img.width + (x + kx);
           // 画像がグレースケールであるため、
-          // RGB値は同一です。
+          // RGB 値は同一です。
           // このサンプルでは赤の値を取得します。
           let val = red(img.get(xpos, ypos));
-          // kernelの合計を蓄積する
-          // kernelは3x3の行列です。
-          // kxとkyには-1、0、1の値があります。
-          // kxとkyに1を加えると、0、1、2が得られます。
-          // それを使用してkernelを反復処理し、
+          // kernel の合計を蓄積する
+          // kernel は3x3の行列です。
+          // kx と ky には-1、0、1の値があります。
+          // kx と ky に1を加えると、0、1、2が得られます。
+          // それを使用して kernel を反復処理し、
           // 累積和を計算できます。
           sum += kernel[ky + 1][kx + 1] * val;
         }
       }
 
-      // edgeImgのピクセル値を設定する
+      // edgeImg のピクセル値を設定する
       edgeImg.set(x, y, color(sum, sum, sum));
     }
   }
 
-  // updatePixels()でedgeImgの変更を書き込む
+  // updatePixels() で edgeImg の変更を書き込む
   edgeImg.updatePixels();
 
-  // 元の画像の右側にedgeImg（エッジ検出した画像）を描画する
+  // 元の画像の右側に edgeImg（エッジ検出した画像）を描画する
   image(edgeImg, img.width, 0);
 }
