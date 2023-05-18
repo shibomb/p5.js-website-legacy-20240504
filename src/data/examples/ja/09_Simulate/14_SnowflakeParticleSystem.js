54338f28b3ba072ea4b2be2476799847df3bc091
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-18 23:33:45 +0900

    fix: put space characters before and behind alphabet words

--- src/data/examples/ja/09_Simulate/14_SnowflakeParticleSystem.js
+++ src/data/examples/ja/09_Simulate/14_SnowflakeParticleSystem.js
@@ -3,7 +3,7 @@
  * @arialabel 赤い背景の上部からランダムなパターンで白い雪片が降り注ぎます。
  * @description 落ちる雪片の動きをシミュレートするパーティクルシステム。
  * 雪片のパーティクルを保持するためのオブジェクト配列を使用しています。
- * Aatish Bhatiaの貢献により作成されました。
+ * Aatish Bhatia の貢献により作成されました。
  */
 
 let snowflakes = []; // 雪片のオブジェクトを保持する配列
@@ -23,14 +23,14 @@ function draw() {
     snowflakes.push(new snowflake()); // 雪片のオブジェクトを追加
   }
 
-  // for..ofループで雪片をループする
+  // for..of ループで雪片をループする
   for (let flake of snowflakes) {
     flake.update(t); // 雪片の位置を更新
     flake.display(); // 雪片を描画
   }
 }
 
-// snowflakeクラス
+// snowflake クラス
 function snowflake() {
   // 座標の初期化
   this.posX = 0;
@@ -43,12 +43,12 @@ function snowflake() {
   this.radius = sqrt(random(pow(width / 2, 2)));
 
   this.update = function(time) {
-    // x位置は円に合わせて動きます。
+    // x 位置は円に合わせて動きます。
     let w = 0.6; // 角速度
     let angle = w * time + this.initialangle;
     this.posX = width / 2 + this.radius * sin(angle);
 
-    // サイズの異なる雪片はわずかに異なるy速度で落ちます。
+    // サイズの異なる雪片はわずかに異なる y 速度で落ちます。
     this.posY += pow(this.size, 0.5);
 
     // 画面の端を超えた雪片を削除します
