d8f719172aec2b71e5c49ce7778cec1f80bda283
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-18 20:14:17 +0900

    fix: put space characters before and behind alphabet words

--- src/data/examples/ja/09_Simulate/01_ParticleSystem.js
+++ src/data/examples/ja/09_Simulate/01_ParticleSystem.js
@@ -17,7 +17,7 @@ function draw() {
   system.run();
 }
 
-// シンプルなParticleクラス
+// シンプルな Particle クラス
 let Particle = function(position) {
   this.acceleration = createVector(0, 0.05);
   this.velocity = createVector(random(-1, 1), random(-1, 0));
