7c1d904184e2bcdfeeaa1d5c5dfc208714a18823
shibomb <shibomb@gmail.com>
2023-06-10 03:11:27 +0900

    Translate サンプル 18 00_03 翻訳しました。

--- src/data/examples/ja/18_Transform/02_Rotate.js
+++ src/data/examples/ja/18_Transform/02_Rotate.js
@@ -1,15 +1,15 @@
 /*
- * @name Rotate
- * @arialabel White square on a dark grey background rotates side to side 
- * @description Rotating a square around the Z axis.
- * To get the results you expect, send the rotate function angle
- * parameters that are values between 0 and PI*2 (TWO_PI which is
- * roughly 6.28). If you prefer to think about angles as degrees
- * (0-360), you can use the radians() method to convert your values.
- * For example: rotate(radians(90)) is identical to the statement
- * rotate(PI/2). In this example, every even numbered second a jitter
- * is added to the rotation. During odd seconds, rotation moves CW and
- * CCW at the speed determined by the last jitter value.
+ * @name 回転
+ * @arialabel 濃い灰色の背景に、白い正方形が回転しています。
+ * @description 正方形をZ軸の周りに回転させます。
+ * 期待通りの結果を得るには、
+ * rotate 関数の angle パラメーターに 0 から PI*2（TWO_PI、およそ 6.28）までの値を指定します。
+ * 角度の単位を「度(0-360)」で考えたい場合は、
+ * radians() 関数を使って値を変換することができます。
+ * 例えば、rotate(radians(90)) は、rotate(PI/2) と同じです。
+ * このサンプルでは
+ * 偶数秒ごとにジッターが回転に加わっています。
+ * 奇数秒では、最後のジッター値によって決定される速度で時計回りおよび反時計回りに回転します。
  */
 
 let angle = 0.0;
@@ -19,26 +19,26 @@ function setup() {
   createCanvas(720, 400);
   noStroke();
   fill(255);
-  //Draw the rectangle from the center and it will also be the
-  //rotate around that center
+  // 中心から長方形を描画するため、
+  // またその中心を回転軸として使用します。
   rectMode(CENTER);
 }
 
 function draw() {
   background(51);
 
-  // during even-numbered seconds (0, 2, 4, 6...) add jitter to
-  // the rotation
+  // 偶数秒ごと（0,2,4,6...）に
+  // ジッターを回転に加えます。
   if (second() % 2 === 0) {
     jitter = random(-0.1, 0.1);
   }
-  //increase the angle value using the most recent jitter value
+  // 最新のジッターを使用して角度値を増加させます。
   angle = angle + jitter;
-  //use cosine to get a smooth CW and CCW motion when not jittering
+  // 動かさないときにスムーズな時計回りおよび反時計回りの動きを得るために余弦(コサイン)を使用します。
   let c = cos(angle);
-  //move the shape to the center of the canvas
+  // 形状をキャンバスの中心へ移動します。
   translate(width / 2, height / 2);
-  //apply the final rotation
+  // 最終的な回転を適用します。
   rotate(c);
   rect(0, 0, 180, 180);
 }
