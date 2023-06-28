c04e8b4930b867b6b9d5668c52b06d3c20e3a3aa
shibomb <shibomb@gmail.com>
2023-06-28 23:00:28 +0900

    Translate: Example 35mobile all initial translate.

--- src/data/examples/en/35_Mobile/04_Tilted_3D_Box.js
+++ src/data/examples/en/35_Mobile/04_Tilted_3D_Box.js
@@ -1,6 +1,6 @@
 /*
- * @name Tilted 3D Box
- * @description Use mobile to tilt a box
+ * @name 傾いた3Dボックス
+ * @description モバイル端末を使って、ボックスを傾けます。
  */
 function setup() {
   createCanvas(displayWidth, displayHeight, WEBGL);
