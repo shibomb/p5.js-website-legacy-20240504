e2287389c4e613cfba3281723af7cab653592225
shibomb <shibomb@gmail.com>
2023-07-19 01:28:29 +0900

    initial translate 01_Form 02_PieChart

--- src/data/examples/ja/01_Form/02_Pie_Chart.js
+++ src/data/examples/ja/01_Form/02_Pie_Chart.js
@@ -1,15 +1,15 @@
 /*
- * @name Pie Chart
- * @arialabel Pie chart on a grey background with the different slices of the pie chart in various shades of grey
- * @description Uses the arc() function to generate a pie chart from the data
- * stored in an array.
+ * @name 円グラフ
+ * @arialabel 灰色の背景に、円グラフのさまざまなスライスをさまざまな色合いの灰色で描いた円グラフがあります。
+ * @description arc() 関数を使用して、配列に格納されたデータから
+ * 円グラフを生成します。
  */
 let angles = [30, 10, 45, 35, 60, 38, 75, 67];
 
 function setup() {
   createCanvas(720, 400);
   noStroke();
-  noLoop(); // Run once and stop
+  noLoop(); // 一度だけ実行して停止します。
 }
 
 function draw() {
